baf264c4d888646a1e43d668085aa07ddbabcdb3
Dan Stroot <dan.stroot@gmail.com>
2018-03-06 12:19:27 -0800

    added dep for vendoring

--- http_util.go
+++ http_util.go
@@ -1,12 +1,3 @@
-//  Copyright (c) 2014 Couchbase, Inc.
-//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
-//  except in compliance with the License. You may obtain a copy of the License at
-//    http://www.apache.org/licenses/LICENSE-2.0
-//  Unless required by applicable law or agreed to in writing, software distributed under the
-//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-//  either express or implied. See the License for the specific language governing permissions
-//  and limitations under the License.
-
 package main
 
 //go:generate go-bindata-assetfs -pkg=main ./static/...
@@ -25,6 +16,7 @@ func staticFileRouter(r *mux.Router, static http.Handler) *mux.Router {
 	// static
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/",
 		myFileHandler{static}))
+
 	// bootstrap ui insists on loading templates from this path
 	r.PathPrefix("/template/").Handler(http.StripPrefix("/template/",
 		myFileHandler{static}))
